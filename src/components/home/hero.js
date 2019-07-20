ad0468859bd130cd2d197f3d71764eb817c558f4
Ted Werbel <werbel.ted@gmail.com>
2019-07-20 02:49:43 -0400

    updated home page

---
+++ src/components/home/hero.js
@@ -0,0 +1,555 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import {
+  FlatButton,
+  Header,
+  PlayButton,
+  ButtonText,
+  ScrollDownIcon,
+  AnimatedText,
+} from '../common';
+
+const Container = styled.div`
+  height: 100vh;
+  width: 90vw;
+  position: relative;
+  background-color: transparent;
+  box-shadow: inset 420px -40px 700px 240px rgba(0, 0, 0, 0.775);
+  @media (max-width: 375px) {
+    width: 100vw;
+  }
+  @media (min-width: 376px) (max-width: 1024px) {
+    width: 90vw;
+  }
+  @media (min-width: 1025px) and (max-width: 1199px) {
+    width: 66vw;
+  }
+  @media (min-width: 1200px) {
+    height: 100vh;
+    width: 100vw;
+  }
+  @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (orientation: landscape) and (-webkit-min-device-pixel-ratio: 2) {
+    height: 105vh;
+    width: 75vw;
+  }
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    height: 100vh;
+  }
+
+  @media only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    height: 100vh;
+  }
+  @media only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait) {
+    height: 100vh;
+  }
+
+  @media only screen and (min-device-width: 375px) and (max-device-width: 667px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    height: 100vh;
+  }
+
+  @media only screen and (min-device-width: 414px) and (max-device-width: 736px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+    height: 100vh;
+  }
+
+  @media only screen and (min-device-width: 375px) and (max-device-width: 812px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+    height: 100vh;
+  }
+  @media screen and (device-width: 412px) and (device-height: 823px) and (-webkit-device-pixel-ratio: 3.5) and (orientation: landscape) {
+    height: 200vh !important;
+  }
+`;
+
+const BackgroundImage = styled.span`
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+`;
+
+const CourseTitle = styled.p`
+  font-family: proxima-nova;
+  font-weight: 800;
+  font-style: black;
+  font-size: 48px;
+  color: #ffffff;
+  opacity: 0.85;
+  letter-spacing: 0.55px;
+  margin-top: 0px;
+  text-transform: uppercase;
+  margin-bottom: 0px;
+  cursor: default;
+  line-height: 48px;
+  @media (max-width: 600px) {
+    font-size: 38px;
+  }
+  @media (max-width: 480px) {
+    font-size: 28px;
+    line-height: 28px;
+  }
+  @media (max-width: 800px) {
+    margin-top: 0px;
+  }
+  @media (min-width: 801px) and (max-width: 1200px) {
+    margin-top: 40px;
+  }
+  @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (orientation: landscape) and (-webkit-min-device-pixel-ratio: 2) {
+    margin-top: 25px;
+  }
+  @media (min-width: 768px) and (max-width: 1024px) and (orientation: portrait) {
+    margin-top: 220px;
+  }
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    font-size: 28px;
+  }
+
+  @media only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    font-size: 28px;
+  }
+
+  @media only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait) {
+    font-size: 24px;
+  }
+
+  @media only screen and (min-device-width: 375px) and (max-device-width: 667px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    font-size: 28px;
+  }
+
+  @media only screen and (min-device-width: 414px) and (max-device-width: 736px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+    font-size: 28px;
+  }
+
+  @media only screen and (min-device-width: 375px) and (max-device-width: 812px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+    font-size: 28px;
+    margin-top: 10px;
+  }
+`;
+const CourseTagline = styled.p`
+  font-family: proxima-nova;
+  font-weight: 700;
+  font-style: black;
+  font-size: 20px;
+  color: #eee;
+  opacity: 0.8;
+  letter-spacing: 5px;
+  margin-top: 10px;
+  text-transform: uppercase;
+  margin-bottom: 0px;
+  cursor: default;
+  @media (max-width: 480px) {
+    font-size: 12px;
+  }
+  @media (max-width: 600px) {
+    font-size: 14px;
+  }
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    font-size: 14px;
+    letter-spacing: 4.2px;
+    margin-top: 0px;
+    position: relative;
+    bottom: 3px;
+  }
+
+  @media only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    font-size: 14px;
+    letter-spacing: 4.2px;
+    margin-top: 0px;
+    position: relative;
+    bottom: 3px;
+  }
+
+  @media only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait) {
+    font-size: 12px;
+    margin-top: 5px;
+    line-height: 15px;
+  }
+
+  @media only screen and (min-device-width: 375px) and (max-device-width: 667px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    font-size: 14px;
+    letter-spacing: 4.2px;
+    margin-top: 0px;
+    position: relative;
+    bottom: 3px;
+  }
+
+  @media only screen and (min-device-width: 414px) and (max-device-width: 736px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+    font-size: 14px;
+    letter-spacing: 4.2px;
+    margin-top: 0px;
+    position: relative;
+    bottom: 3px;
+  }
+
+  @media only screen and (min-device-width: 375px) and (max-device-width: 812px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+    font-size: 14px;
+    letter-spacing: 4.2px;
+    margin-top: 0px;
+    position: relative;
+    bottom: 3px;
+  }
+`;
+const GenericTagline = styled.p`
+  font-family: roboto-condensed, sans-serif;
+  font-weight: 400;
+  font-size: 22px;
+  color: #ffffff;
+  opacity: 0.9;
+  margin-top: 30px;
+  margin-bottom: 0px;
+  cursor: default;
+  line-height: 30px;
+  max-width: 550px;
+  @media (max-width: 900px) {
+    font-size: 20px;
+  }
+  @media (max-width: 600px) {
+    font-size: 18px;
+  }
+  @media (max-width: 480px) {
+    font-size: 15px;
+    line-height: 20px;
+    margin-top: 20px;
+  }
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    font-size: 14px;
+    line-height: 18px;
+    margin-top: 15px;
+  }
+
+  @media only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    font-size: 14px;
+    line-height: 18px;
+    margin-top: 15px;
+  }
+
+  @media only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait) {
+    font-size: 13px;
+    margin-top: 10px;
+    line-height: 15px;
+  }
+
+  @media only screen and (min-device-width: 375px) and (max-device-width: 667px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    font-size: 14px;
+    line-height: 18px;
+    margin-top: 15px;
+  }
+
+  @media only screen and (min-device-width: 414px) and (max-device-width: 736px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+    font-size: 14px;
+    line-height: 18px;
+    margin-top: 15px;
+  }
+
+  @media only screen and (min-device-width: 375px) and (max-device-width: 812px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+    font-size: 14px;
+    line-height: 18px;
+    margin-top: 15px;
+  }
+`;
+const SubscriptionTagline = styled.p`
+  font-family: adobe-caslon-pro, sans-serif;
+  font-weight: 400;
+  font-size: 24px;
+  color: #f5f5f5;
+  margin-top: 60px;
+  margin-bottom: 0px;
+  cursor: default;
+  line-height: 30px;
+  @media (max-width: 480px) {
+    font-size: 16px;
+    margin-top: 20px;
+  }
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    font-size: 14px;
+    margin-top: 20px;
+  }
+
+  @media only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    font-size: 14px;
+    margin-top: 30px;
+  }
+
+  @media only screen and (min-device-width: 375px) and (max-device-width: 667px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    font-size: 14px;
+    margin-top: 20px;
+  }
+
+  @media only screen and (min-device-width: 414px) and (max-device-width: 736px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+    font-size: 14px;
+    margin-top: 20px;
+  }
+
+  @media only screen and (min-device-width: 375px) and (max-device-width: 812px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+    font-size: 14px;
+    margin-top: 20px;
+  }
+`;
+
+const Price = styled.p`
+  font-family: adobe-caslon-pro, sans-serif;
+  font-weight: 600;
+  font-size: 42px;
+  color: #fff;
+  margin-top: 25px;
+  margin-bottom: 0px;
+  cursor: default;
+  line-height: 30px;
+  max-width: 550px;
+  @media (max-width: 480px) {
+    font-size: 32px;
+    margin-top: 10px;
+  }
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    font-size: 32px;
+    margin-top: 5px;
+  }
+
+  @media only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    font-size: 32px;
+    margin-top: 5px;
+  }
+
+  @media only screen and (min-device-width: 375px) and (max-device-width: 667px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    font-size: 32px;
+    margin-top: 5px;
+  }
+
+  @media only screen and (min-device-width: 414px) and (max-device-width: 736px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+    font-size: 32px;
+    margin-top: 5px;
+  }
+
+  @media only screen and (min-device-width: 375px) and (max-device-width: 812px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+    font-size: 32px;
+    margin-top: 5px;
+  }
+`;
+
+const FeaturedCourse = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding-left: 60px;
+  padding-right: 60px;
+  margin-top: 60px;
+  @media only screen and (min-width: 1024px) and (max-height: 1366px) {
+    flex-direction: column-reverse;
+  }
+  @media (max-width: 600px) {
+    flex-direction: column-reverse;
+    margin-top: 0px;
+  }
+  @media (max-width: 480px) {
+    padding-left: 40px;
+    padding-right: 40px;
+  }
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    margin-top: 30px;
+  }
+
+  @media only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    margin-top: 30px;
+  }
+
+  @media only screen and (min-device-width: 375px) and (max-device-width: 667px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    margin-top: 30px;
+  }
+
+  @media only screen and (min-device-width: 414px) and (max-device-width: 736px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+    margin-top: 100px;
+  }
+
+  @media only screen and (min-device-width: 375px) and (max-device-width: 812px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+    margin-top: 30px;
+  }
+`;
+
+const PlayButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 70%;
+  padding-top: 100px;
+  padding-bottom: 60px;
+  @media only screen and (min-width: 1024px) and (max-height: 1366px) {
+    justify-content: flex-start;
+  }
+  @media (max-width: 600px) {
+    justify-content: flex-start;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    transform: scale(0.6) translateX(-110px);
+  }
+  @media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+    padding-top: 0px;
+    padding-bottom: 0px;
+  }
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    padding-bottom: 0px;
+    padding-top: 0px;
+  }
+
+  @media only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    padding-bottom: 0px;
+    padding-top: 0px;
+  }
+  @media only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait) {
+    padding-bottom: 5px;
+    padding-top: 10px;
+    transform: scale(0.6) translateX(-92.5px);
+  }
+
+  @media only screen and (min-device-width: 375px) and (max-device-width: 667px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    padding-bottom: 0px;
+    padding-top: 0px;
+  }
+
+  @media only screen and (min-device-width: 414px) and (max-device-width: 736px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+    padding-bottom: 0px;
+    padding-top: 0px;
+  }
+
+  @media only screen and (min-device-width: 375px) and (max-device-width: 812px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+    padding-bottom: 0px;
+    padding-top: 0px;
+  }
+  @media only screen and (min-device-width: 375px) and (max-device-width: 812px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: portrait) {
+    padding-bottom: 30px;
+    padding-top: 50px;
+  }
+  @media (max-width: 800px) {
+    transform: scale(0.7) translateX(0px);
+  }
+  @media (max-width: 600px) {
+    transform: scale(0.7) translateX(-55px);
+  }
+`;
+
+const HeroButtons = styled.div`
+  display: flex;
+  margin-left: 60px;
+  margin-right: 120px;
+  margin-top: 22.5px;
+  @media (max-width: 600px) {
+    flex-direction: column;
+    justify-content: space-between;
+    height: 95px;
+  }
+  @media (max-width: 480px) {
+    margin-left: 40px;
+  }
+  @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    margin-top: 8px;
+  }
+
+  @media only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    margin-top: 8px;
+  }
+
+  @media only screen and (min-device-width: 320px) and (max-device-width: 568px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait) {
+    margin-top: 8px;
+  }
+
+  @media only screen and (min-device-width: 375px) and (max-device-width: 667px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    margin-top: 8px;
+  }
+
+  @media only screen and (min-device-width: 414px) and (max-device-width: 736px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+    margin-top: 8px;
+  }
+
+  @media only screen and (min-device-width: 375px) and (max-device-width: 812px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+    margin-top: 8px;
+  }
+`;
+export default class Hero extends Component {
+  componentDidMount() {
+    // eslint-disable-next-line no-unused-vars
+    const AnimatedTextHandler = new AnimatedText({
+      targetId: 'courseTitle', // text element id
+      texts: ['Metro Boomin', 'BOI-1DA', 'Southside', 'TM88'], // data array
+      changeInterval: 6000, // delay between transitions (ms)
+      updateInterval: 19, // transition time (ms)
+      autoplay: true,
+    });
+    // eslint-disable-next-line no-unused-vars
+    const AnimatedTextHandler2 = new AnimatedText({
+      targetId: 'courseTagline', // text element id
+      texts: [
+        'Teaches Trap Production',
+        'Teaches Trap Drums',
+        'Teaches Sampling Techniques',
+        'Teaches FL Studio',
+      ], // data array
+      changeInterval: 6000, // delay between transitions (ms)
+      updateInterval: 10, // transition time (ms)
+      autoplay: true,
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.AnimatedTextHandler && this.AnimatedTextHandler2) {
+      this.AnimatedTextHandler.stop();
+      this.AnimatedonTextHandler2.stop();
+    }
+  }
+
+  render() {
+    const { showRegistrationModal, toggleTrailerModal } = this.props;
+    return (
+      <Container>
+        {/* Header Logo, Login and Instructor Buttons */}
+        <Header />
+        {/* Featured Course */}
+        <FeaturedCourse>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <CourseTitle id="courseTitle" className="disable-selection" />
+            <CourseTagline id="courseTagline" className="disable-selection" />
+
+            <GenericTagline className="disable-selection">
+              Online courses, samples, mentoring and many other perks offered by the world's most
+              talented producers.
+            </GenericTagline>
+
+            <SubscriptionTagline className="disable-selection">
+              Subscriptions starting at
+            </SubscriptionTagline>
+            <Price className="disable-selection">$5 / monthly</Price>
+          </div>
+          <PlayButtonContainer>
+            <PlayButton onClick={toggleTrailerModal} />
+          </PlayButtonContainer>
+        </FeaturedCourse>
+        <HeroButtons>
+          <FlatButton
+            onClick={() => showRegistrationModal(true)}
+            style={{ width: 200, marginRight: 20 }}
+          >
+            <ButtonText>Sign Up</ButtonText>
+          </FlatButton>
+          <FlatButton
+            onClick={toggleTrailerModal}
+            style={{ width: 220, backgroundColor: 'rgba(255, 255, 255, 0.275' }}
+          >
+            <ButtonText>Learn More</ButtonText>
+          </FlatButton>
+        </HeroButtons>
+
+        <ScrollDownIcon />
+        <div className="cb-slideshow">
+          <li>
+            <BackgroundImage />
+          </li>
+
+          <li>
+            <BackgroundImage />
+          </li>
+
+          <li>
+            <BackgroundImage />
+          </li>
+          <li>
+            <BackgroundImage />
+          </li>
+        </div>
+      </Container>
+    );
+  }
+}
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,26 +1,214 @@
-import React from 'react';
-import styled from 'styled-components';
+/* eslint-disable max-len */
+/* eslint-disable react/no-unescaped-entities */
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import HeroImage from '../../assets/hero-image-3.jpeg';
+import { showAuthModal, showRegistrationModal } from '../../actions';
+import {
+  AuthModal,
+  TrailerModal,
+  Footer,
+  HorizontalScrollView,
+  InstructorCard,
+  SectionTitle,
+  CourseCard,
+  CardList,
+  MusicIcon,
+  SubscribeIcon,
+  ShareIcon,
+  ValuePropCard,
+  ValuePropositions,
+} from '../common';
 
-const Hero = styled.div`
-  height: 80vh;
-  width: 100%;
-  background-image: url(${HeroImage});
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-color: #555;
-  box-shadow: inset 71px -29px 246px 190px rgba(0, 0, 0, 0.81);
-`;
+// Components
+import Hero from './hero';
 
-export const Home = () => {
-  return (
-    <div>
-      <Hero />
-      <p>home page</p>
-    </div>
-  );
-};
+// Image Assets
+import HeroImage3 from '../../assets/hero-image-3.jpeg';
+import HeroImage4 from '../../assets/hero-image-8.jpg';
+import HeroImage5 from '../../assets/hero-image-10.png';
+import HeroImage from '../../assets/hero-image-12.jpg';
 
-export default Home;
+// componentDidMount() {
+//   auth().onAuthStateChanged(user => {
+//     return user ? this.setState({ user }) : this.setState({ user: null });
+//   });
+// }
+
+// async handleFunction() {
+//   const testFunction = functions().httpsCallable('testFunction');
+//   const response = await testFunction({ message: 'Howdy!' });
+//   return console.log(response);
+// }
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showTrailerModal: false,
+      showFloatingButton: false,
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      if (document.documentElement.scrollTop > 1024 || document.body.scrollTop > 1024) {
+        this.setState({ showFloatingButton: true });
+      } else {
+        this.setState({ showFloatingButton: false });
+      }
+    }, 1000);
+  }
+
+  render() {
+    // eslint-disable-next-line no-shadow
+    const { showRegistrationModal } = this.props;
+    const { showTrailerModal, showFloatingButton } = this.state;
+    return (
+      <div style={{ overflowX: 'hidden' }}>
+        {/* Animated Hero Image and Featured Courses */}
+        <Hero
+          showRegistrationModal={showRegistrationModal}
+          toggleTrailerModal={() => this.setState({ showTrailerModal: !showTrailerModal })}
+        />
+
+        {/* Recently Added Courses */}
+        <div style={{ background: '#fff', paddingTop: 40 }}>
+          <SectionTitle style={{ marginTop: 0 }}>Recently Added</SectionTitle>
+          <CardList mini>
+            <CourseCard
+              backgroundImage={HeroImage3}
+              title="BOI-1DA"
+              tagline="Teaches Drumming"
+              mini
+            />
+            <CourseCard
+              backgroundImage={HeroImage4}
+              title="Southside"
+              tagline="Teaches Trap Production"
+              mini
+            />
+            <CourseCard
+              backgroundImage={HeroImage5}
+              title="London on da track"
+              tagline="Teaches FL Studio"
+              mini
+            />
+            <CourseCard
+              backgroundImage={HeroImage}
+              title="London on da track"
+              tagline="Teaches FL Studio"
+              mini
+            />
+          </CardList>
+
+          {/* Value Propositions */}
+          <ValuePropositions>
+            <ValuePropCard
+              icon={<MusicIcon />}
+              title="Learn"
+              description="Enroll in courses taught by world-class producers, sound engineers and music
+                  artists."
+            />
+            <ValuePropCard
+              icon={<SubscribeIcon />}
+              title="Subscribe"
+              description="Support your favorite producers and gain early-access to courses, sample packs and
+                  more."
+            />
+            <ValuePropCard
+              icon={<ShareIcon />}
+              title="Engage"
+              description="Share your musical works with peers and instructors for constructive feedback."
+              style={{ borderRight: '0px', borderBottom: '0px' }}
+            />
+          </ValuePropositions>
+
+          {/* Instructors */}
+          <SectionTitle>Instructors</SectionTitle>
+          <HorizontalScrollView>
+            <InstructorCard backgroundImage={HeroImage3} name="BOI-1DA" />
+            <InstructorCard backgroundImage={HeroImage4} name="Southside" />
+            <InstructorCard backgroundImage={HeroImage5} name="London on da track" />
+            <InstructorCard backgroundImage={HeroImage3} name="BOI-1DA" />
+            <InstructorCard backgroundImage={HeroImage4} name="Southside" />
+            <InstructorCard backgroundImage={HeroImage5} name="London on da track" />
+            <InstructorCard backgroundImage={HeroImage5} name="London on da track" />
+            <InstructorCard backgroundImage={HeroImage5} name="London on da track" />
+          </HorizontalScrollView>
+
+          {/* Available Courses */}
+          <SectionTitle>Available Courses</SectionTitle>
+          <CardList>
+            <CourseCard backgroundImage={HeroImage3} title="BOI-1DA" tagline="Teaches Drumming" />
+
+            <CourseCard
+              backgroundImage={HeroImage4}
+              title="Southside"
+              tagline="Teaches Trap Production"
+            />
+            <CourseCard
+              backgroundImage={HeroImage5}
+              title="London on da track"
+              tagline="Teaches FL Studio"
+            />
+            <CourseCard
+              backgroundImage={HeroImage}
+              title="London on da track"
+              tagline="Teaches FL Studio"
+            />
+          </CardList>
+
+          {/* Coming Soon */}
+          <SectionTitle>Coming Soon</SectionTitle>
+          <CardList style={{ paddingBottom: 80 }}>
+            <CourseCard
+              backgroundImage={HeroImage4}
+              title="Southside"
+              tagline="Teaches Trap Production"
+              disabled
+            />
+            <CourseCard
+              backgroundImage={HeroImage5}
+              title="London on da track"
+              tagline="Teaches FL Studio"
+              disabled
+            />
+            <CourseCard
+              backgroundImage={HeroImage}
+              title="London on da track"
+              tagline="Teaches FL Studio"
+              disabled
+            />
+          </CardList>
+        </div>
+
+        <Footer />
+        {/* Modals */}
+        <AuthModal />
+        <TrailerModal
+          open={showTrailerModal}
+          close={() => this.setState({ showTrailerModal: false })}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  user: state.auth.user,
+  firstName: state.auth.firstName,
+  lastName: state.auth.lastName,
+  email: state.auth.email,
+  password: state.auth.password,
+  confirmPassword: state.auth.confirmPassword,
+  error: state.auth.error,
+  loading: state.auth.loading,
+  showModal: state.auth.showModal,
+});
+
+export default connect(
+  mapStateToProps,
+  { showAuthModal, showRegistrationModal },
+)(Home);
