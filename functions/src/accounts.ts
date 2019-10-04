1e54ef05c3fe677f6566ae70bda9a3ce45f0dfaf
Ted Werbel <werbel.ted@gmail.com>
2019-10-03 23:30:56 -0400

    Created accounts endpoint / functions

--- functions/src/accounts.ts
+++ functions/src/accounts.ts
@@ -1,38 +1,71 @@
 import * as functions from 'firebase-functions';
 import { db } from './config';
+import { catchErrors, assertEmail } from './helpers';
+
+const saveEmail = async (email: string, context: any) => {
+  // 1. Check if auth context is available, if so - throw error
+  if (context.auth) {
+    throw new functions.https.HttpsError('permission-denied', 'User already has an account');
+  }
+  // 2. Check if email already exists in emails collection, if so - throw error
+  const snapshot = await db
+    .collection('emails')
+    .where('email', '==', email)
+    .get();
+
+  if (!snapshot.empty) {
+    throw new functions.https.HttpsError('permission-denied', 'Email already added');
+  } else {
+    // 3. Else, add email to a new doc in emails collection
+    await db
+      .collection('emails')
+      .doc(email)
+      .set({ email });
+  }
+};
+
+// DEPLOYABLE FUNCTIONS //
+
+export const saveAnonymousUser = functions.https.onCall(async (data, context) => {
+  const email = assertEmail(data, 'email');
+
+  return catchErrors(saveEmail(email, context));
+});
 
 export const createUserEvent = functions.auth.user().onCreate(async user => {
   const { uid, email } = user;
+  // Delete email if previously saved in database
+  await db
+    .collection('emails')
+    .doc(email)
+    .delete();
   // checks if user had an old account and restores purchases
-  db.collection('accounts')
+  const snapshot = await db
+    .collection('accounts')
     .where('email', '==', email)
-    .get()
-    .then(snapshot => {
-      if (!snapshot.empty) {
-        console.log('Old account found!');
-        snapshot.forEach(async doc => {
-          // [Start: Batch write purchase recovery info and delete old account]
-          const purchaseRecoveryBatch = db.batch();
+    .get();
 
-          // Add purchases to new account object if found in old account
-          const newAccountsRef = await db.collection('accounts').doc(uid);
-          purchaseRecoveryBatch.set(
-            newAccountsRef,
-            { purchases: doc.data().purchases || null },
-            { merge: true },
-          );
+  if (!snapshot.empty) {
+    console.log('Old account found!');
+    snapshot.forEach(async doc => {
+      // [Start: Batch write purchase recovery info and delete old account]
+      const purchaseRecoveryBatch = db.batch();
 
-          // Delete old account object
-          const oldAccountRef = await db.collection('accounts').doc(doc.data().uid);
-          purchaseRecoveryBatch.delete(oldAccountRef);
-          // Run batch write
-          await purchaseRecoveryBatch.commit();
-        });
-      }
-    })
-    .catch(err => {
-      console.log('No old account found', err);
+      // Add purchases to new account object if found in old account
+      const newAccountsRef = await db.collection('accounts').doc(uid);
+      purchaseRecoveryBatch.set(
+        newAccountsRef,
+        { purchases: doc.data().purchases || null },
+        { merge: true },
+      );
+
+      // Delete old account object
+      const oldAccountRef = await db.collection('accounts').doc(doc.data().uid);
+      purchaseRecoveryBatch.delete(oldAccountRef);
+      // Run batch write
+      await purchaseRecoveryBatch.commit();
     });
+  }
 
   // writes uid, email, username and roles to db atomically
   const batch = db.batch();
