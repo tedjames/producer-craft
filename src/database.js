98332eb8e91afa91541e08d43838715d6e962488
Ted Werbel <tedwerbel@Teds-MBP.lan>
2019-12-10 03:04:22 -0500

    added database persistence

--- src/database.js
+++ src/database.js
@@ -4,5 +4,10 @@ import firebaseConfig from './firebaseConfig';
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const db = firebaseApp.firestore();
+const perf = firebaseApp.performance();
 
-export { db };
+db.enablePersistence({ synchronizeTabs: true })
+  .then(() => console.log('Persistence Enabled'))
+  .catch(err => console.log('Unable to configure persistence: ', err));
+
+export { db, perf };
